c9f4eaa9a925f1fb7fc1273cca86d2646d3ca359
leonidas <leonidas.luteganya@gmail.com>
2025-06-05 23:11:29 +0300

    update requirements

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Your app URLs
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Root path for Swagger
+    # path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Root path for Swagger
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
